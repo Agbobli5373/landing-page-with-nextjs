776c984541a7faaec68b766215ed990f498f17f9
agbobli isaac <agbobliisaackwadzo@gmail.com>
2023-01-05 04:20:21 +0000

    Add social media icon to the header

--- components/_child/newsLetter.tsx
+++ components/_child/newsLetter.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
 
 export default function newsLetter() {
+
   return (
-    <div className='flex flex-col justify-center py-5 gap-3'>
-        <input type="email" placeholder="" className="mt-1 block w-60 px-3 py-2 bg-white border border-slate-100
+    <div className='flex flex-col justify-center py-5 gap-3 mx-auto'>
+        <input type="email" placeholder="your@email.com" className="mt-1 block w-60 px-3 py-2 bg-white border border-slate-100
                               rounded-full text-sm shadow placeholder-slate-500 ;
                               focus:outline-none focus:border-sky-500 focus:ring-sky-500" />
-        <button className='p-2 outline text-white'>Subscribe</button>
+        <button className='p-1 outline text-white hover:text-black'>Subscribe</button>
+
     </div>
 
   )
--- components/footer.tsx
+++ components/footer.tsx
@@ -2,9 +2,12 @@ import Link from "next/link" ;
 import NewsLetter from "./_child/newsLetter";
 import {FaYoutube,FaFacebook,FaTwitter,FaWhatsapp} from "react-icons/fa" ;
 export default function footer() {
+    let year = new Date().getFullYear();
   return (
     <div className="bg-gray-500 mx-auto py-10 shadow flex flex-col justify-center items-center">
       <NewsLetter/>
+      <p className="py-3 text-gray-400 text-center">{`Copyright ©${year} All rights reserved | This template is made with  by`}</p>
+                     <p className="text-gray-400 text-center">Terms and Conditions</p>
       <div className="w-96 order-3 flex justify-center">
            <div className="flex gap-6">
            <Link href={"/"}><FaFacebook color="#ffffff"/></Link>
